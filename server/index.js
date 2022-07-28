f12895e42dced54e2021324d8a210a400ae40e10
Arpit Surana <suranaarpit34@gmail.com>
2022-07-28 16:39:13 +0530

    google oauth backend

--- server/index.js
+++ server/index.js
@@ -1,14 +1,31 @@
 const express = require("express");
 require("dotenv").config({ path: "./.env" });
 const connection = require("./Database/db");
+const cookiesSession = require("cookie-session");
 const cors = require("cors");
 const helmet = require("helmet");
-const { userRouter } = require("./routes/user.route");
-
+const passport = require("passport");
+const googlePassportSetUp = require("./Auth/Passport");
+const authRouter = require("./routes/auth");
 const server = express();
 
 server.use(express.urlencoded({ extended: true }));
-server.use(cors());
+server.use(
+  cookiesSession({
+    name: "session",
+    keys: ["Arpit"],
+    maxAge: 24 * 60 * 60 * 100,
+  })
+);
+server.use(passport.initialize());
+server.use(passport.session());
+server.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET, POST, PUT, DELETE",
+    credentials: true,
+  })
+);
 server.use(helmet());
 server.use(express.json());
 
@@ -16,7 +33,7 @@ server.get("/", (req, res) => {
   res.send(`API Server started on localhost:${PORT}`);
 });
 
-server.use("/", userRouter);
+server.use("/auth", authRouter);
 
 const PORT = process.env.PORT || 8080;
 
