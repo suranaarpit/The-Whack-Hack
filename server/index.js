2bdc7838414840d127f3b1b4721f38610c59faba
Arpit Surana <suranaarpit34@gmail.com>
2022-07-28 17:03:01 +0530

    updated index.js

--- server/index.js
+++ server/index.js
@@ -7,6 +7,9 @@ const helmet = require("helmet");
 const passport = require("passport");
 const googlePassportSetUp = require("./Auth/Passport");
 const authRouter = require("./routes/auth");
+const authRoutes = require("./routes/auth");
+const messageRoutes = require("./routes/messages");
+const socket = require("socket.io");
 const server = express();
 
 server.use(express.urlencoded({ extended: true }));
@@ -34,11 +37,34 @@ server.get("/", (req, res) => {
 });
 
 server.use("/auth", authRouter);
+server.use("/api/auth", authRoutes);
+server.use("/api/messages", messageRoutes);
 
 const PORT = process.env.PORT || 8080;
 
-server.listen(PORT, async () => {
+const NEW_SERVER = server.listen(PORT, async () => {
   await connection;
   console.log("Connected to Database");
   console.log(`🌎 started on http://localhost:${PORT}/`);
+});
+const io = socket(NEW_SERVER, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+    }
+  });
 });
