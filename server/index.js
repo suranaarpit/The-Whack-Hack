4795c33a77fef8026e6f788572c271e6c0ea3c4e
R4rohitm <r4rohitm45@gmail.com>
2022-07-28 17:43:00 +0530

    update

--- server/index.js
+++ server/index.js
@@ -1,12 +1,34 @@
 const express = require("express");
+require("dotenv").config({ path: "./.env" });
 const connection = require("./Database/db");
+const cookiesSession = require("cookie-session");
 const cors = require("cors");
 const helmet = require("helmet");
-
+const passport = require("passport");
+const googlePassportSetUp = require("./Auth/Passport");
+const authRouter = require("./routes/auth");
+// const authRoutes = require("./routes/auth");
+// const messageRoutes = require("./routes/messages");
+const socket = require("socket.io");
 const server = express();
 
 server.use(express.urlencoded({ extended: true }));
-server.use(cors());
+server.use(
+  cookiesSession({
+    name: "session",
+    keys: ["Arpit"],
+    maxAge: 24 * 60 * 60 * 100,
+  })
+);
+server.use(passport.initialize());
+server.use(passport.session());
+server.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET, POST, PUT, DELETE",
+    credentials: true,
+  })
+);
 server.use(helmet());
 server.use(express.json());
 
@@ -14,10 +36,35 @@ server.get("/", (req, res) => {
   res.send(`API Server started on localhost:${PORT}`);
 });
 
+server.use("/auth", authRouter);
+// server.use("/api/auth", authRoutes);
+// server.use("/api/messages", messageRoutes);
+
 const PORT = process.env.PORT || 8080;
 
-server.listen(PORT, async () => {
+const NEW_SERVER = server.listen(PORT, async () => {
   await connection;
   console.log("Connected to Database");
   console.log(`🌎 started on http://localhost:${PORT}/`);
+});
+const io = socket(NEW_SERVER, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+    }
+  });
 });
